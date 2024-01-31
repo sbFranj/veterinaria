ca27e85850094ba5f0f60e33f84573de1ce93d52
Fran Sánchez <115450163+sbFranj@users.noreply.github.com>
2024-01-31 19:55:31 +0100

    Delete models directory

--- models/gato.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const GatoSchema = new Schema({
-    name: String,
-    age: Number
-})
-
-module.exports = mongoose.model("Gato", GatoSchema);
